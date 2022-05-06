41ec29b961a47613e17326cf8348b60a5e9614b5
Luis Carlos <lucaldev22@gmail.com>
2022-05-05 23:45:54 -0300

    Dia 2 avanço no frontend

---
+++ web/src/components/WidgetForm/index.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { CloseButton } from '../CloseButton'
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+import { FeedbackTypeStep } from './steps/feedbackTypeStep'
+import { FeedbackContentStep } from './steps/feedbackContentStep'
+
+
+export const feedbackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        }
+    },
+    IDEIA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        }
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'Imagem de um balão de pensamento'
+        }
+    }
+}
+
+export type feedbackType = keyof typeof feedbackTypes
+
+export function WidgetForm() {
+    const [feedbackType, setFeedbackType]= useState<feedbackType | null>(null)
+
+    function handleRestartFeedback(){
+        setFeedbackType(null)
+    }
+
+    return (
+        <div className='bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto'>
+  
+            {!feedbackType?(
+
+            <FeedbackTypeStep 
+            onFeedbackTypeChanged = {setFeedbackType}
+            />):
+
+            <FeedbackContentStep 
+            feedbackType = {feedbackType}
+            handleRestart = {handleRestartFeedback}
+
+            />}
+            <footer className='text-sx text-neutral-400'>
+                Feito por Lucal e Rocketseat
+            </footer>
+
+        </div>
+    )
+}
---
+++ web/src/components/WidgetForm/steps/feedbackContentStep.tsx
@@ -0,0 +1,58 @@
+import { CloseButton } from "../../CloseButton"
+import { feedbackType, feedbackTypes } from ".."
+import { ArrowLeft} from "phosphor-react";
+import {ScreenShotButton} from './screenShotButton'
+
+interface FeedbackContentStepProps {
+    feedbackType: feedbackType
+    handleRestart: () => void;
+}
+
+export function FeedbackContentStep({ feedbackType, handleRestart }: FeedbackContentStepProps) {
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
+    return (
+        <>
+            <header>
+                <button
+                    type="button"
+                    className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+                    onClick={handleRestart}
+                >
+                    <ArrowLeft
+                        weight="bold"
+                        className="w-4 h-4"
+                    />
+                </button>
+                <span className='text-xl leading-6 flex'>
+                    <img
+                        src={feedbackTypeInfo.image.source}
+                        alt={feedbackTypeInfo.image.alt}
+                        className='w-6 h-6 flex items-center gap-2'
+                    />
+                    {feedbackTypeInfo.title}
+
+
+                </span>
+
+                <CloseButton />
+            </header>
+            <form className="my-4 w-full">
+                <textarea
+                className="min-w-[304px] w-full h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+                placeholder="Conte com detalhes o que está acontecendo"
+                />
+            <footer className="flex gap-2 mt-2">
+                <ScreenShotButton />
+                <button
+                type="submit"
+                className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+                >
+                    Enviar feedback
+                </button>
+            </footer>
+            </form>
+            
+
+        </>
+    )
+}
