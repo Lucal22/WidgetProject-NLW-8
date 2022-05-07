c695aca174e9afed1d9587314bac7b27d964357c
Luis Carlos <lucaldev22@gmail.com>
2022-05-07 00:54:09 -0300

    Dia 3: Backend finalizado.

---
+++ server/src/routes.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
+import { PrismaFeedbackRepository } from './repositories/prisma/prisma-feedbacks-repository'
+import { NodemailerMailAdapter } from './adapters/nodemailer/nodemailer-mail-adapter'
+
+
+const routes = express.Router()
+
+
+
+export default routes.post('/feedbacks', async (req, res) => {
+    const { type, comment, screenshot } = req.body
+
+    const prismaFeedbackRepository = new  PrismaFeedbackRepository()
+    const nodemailerAdapter = new NodemailerMailAdapter()
+
+    const submitFeedbackUseCase = new SubmitFeedbackUseCase(
+        prismaFeedbackRepository,
+        nodemailerAdapter
+        )
+
+    await submitFeedbackUseCase.execute(
+        {
+            type,
+            comment,
+            screenshot
+        }
+    )
+
+    return res.status(201).send()
+})
