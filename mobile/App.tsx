a8cc7edf15dbcb2ea3a0b70441ceaf02b9104ad6
Luis Carlos <lucaldev22@gmail.com>
2022-05-10 20:00:49 -0300

    Dia 4: Termino do frontend mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
-import { Widget } from './src/components/Widget';
+import  Widget  from './src/components/Widget';
 import { theme } from './src/theme';
 import AppLoading from 'expo-app-loading';
 import { useFonts, Inter_400Regular, Inter_500Medium } from '@expo-google-fonts/inter';
+
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -13,18 +14,18 @@ export default function App() {
 
   if (!fontsLoaded) {
     return <AppLoading />;
-  }
+  } 
   return (
     <View style={{
       flex: 1,
       backgroundColor: theme.colors.background
     }}>
-      <Widget />
       <StatusBar
         style="light"
         backgroundColor='transparent'
         translucent
       />
+      <Widget />
     </View>
   );
 }
--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -1,13 +1,85 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import { ChatTeardropDots } from 'phosphor-react-native'
+import 'react-native-gesture-handler';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
 
 import { styles } from './styles';
+import { theme } from '../../theme';
+import { Options } from '../Options';
+import { Form } from '../Form';
 
-export function Widget() {
+
+import { feedbackTypes } from '../../utils/feedbackTypes';
+import { Success } from '../Success';
+
+export type FeedbackType = keyof typeof feedbackTypes ;
+
+function Widget() {
+  const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+  const [feedbackSent, setFeebackSent] = useState(false)
+
+  const bottomSheetRef = useRef<BottomSheet>(null)
+
+  function handleOpen(){
+    bottomSheetRef.current?.expand();
+  }
+
+  function handleRestart(){
+    setFeedbackType(null);
+    setFeebackSent(false);
+  }
+  
+  function handleFeedbackSent(){
+    setFeebackSent(true);
+  }
   return (
     <>
-    <TouchableOpacity>
-    </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={handleOpen }
+      >
+        <ChatTeardropDots
+          size={24}
+          weight='bold'
+          color={theme.colors.text_on_brand_color}
+        />
+      </TouchableOpacity>
+
+      <BottomSheet
+        ref={bottomSheetRef}
+      snapPoints={[1, 280]}
+      backgroundStyle={styles.modal}
+      handleIndicatorStyle={styles.indicator}
+      >
+        {
+          feedbackSent?
+          <Success
+          onSendAnotherFeedback = {handleRestart}
+          />
+          :
+          <>
+          
+          {feedbackType?
+          <Form 
+          feedbackType={feedbackType}
+          onFeedbackCanceled={handleRestart}
+          onFeedbackSent = {handleFeedbackSent}
+          />
+          :
+          <Options
+          onFeedbackTypeChanged={setFeedbackType}
+          />
+        }
+        
+          </>
+        }
+        
+
+    </BottomSheet>
     </>
   );
 }
+
+export default gestureHandlerRootHOC(Widget);
